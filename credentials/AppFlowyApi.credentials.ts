b281ad38b6e55af57e1c3b8f4a68bdb3ca109918
octionic <octionicsolutions@gmail.com>
2025-02-05 22:38:13 +0100

    update description, add credential

---
+++ credentials/AppFlowyApi.credentials.ts
@@ -0,0 +1,35 @@
+import type { ICredentialType, INodeProperties } from 'n8n-workflow';
+
+// https://github.com/AppFlowy-IO/AppFlowy-Docs/blob/main/documentation/appflowy-cloud/openapi/Apis/OAuthApi.md#gotruetoken
+
+export class AppFlowyApi implements ICredentialType {
+	name = 'appflowyApi';
+
+	displayName = 'AppFlowy API';
+
+	documentationUrl = '';
+
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Host',
+			name: 'host',
+			type: 'string',
+			default: 'https://beta.appflowy.cloud',
+		},
+		{
+			displayName: 'Username',
+			name: 'username',
+			type: 'string',
+			default: '',
+		},
+		{
+			displayName: 'Password',
+			name: 'password',
+			type: 'string',
+			default: '',
+			typeOptions: {
+				password: true,
+			},
+		},
+	];
+}
